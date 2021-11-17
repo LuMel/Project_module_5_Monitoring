40df12d76e065c526b9810dee616e51434f5f293
Luis Melgar <luis.melgar@scrm.lidl>
2021-11-17 23:46:18 +0100

    diagnostics.py completed

--- diagnostics.py
+++ diagnostics.py
@@ -1,11 +1,15 @@
 
 import pandas as pd
 import numpy as np
+import subprocess
 import timeit
 from typing import List, Tuple
+from io import BytesIO
 import os
 import json
 import pickle
+from training import train_model
+from ingestion import merge_multiple_dataframe
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
@@ -17,19 +21,24 @@ deployment_path = os.path.join(config['prod_deployment_path'])
 
 
 ##################Function to get model predictions
-def model_predictions(data: pd.DataFrame) -> np.ndarray:
+def model_predictions() -> np.ndarray:
     # read the deployed model and a test dataset, calculate predictions
     with open(deployment_path + "/" + "trainedmodel.pkl", 'rb') as model_pickle:
         model_ = pickle.load(model_pickle)
     # read scaler
     with open(deployment_path + "/" + "scscaler.pkl", 'rb') as pickle_sc:
         sc_ = pickle.load(pickle_sc)
+    
+    test_data = pd.read_csv(test_data_path + "/" + "testdata.csv")
+    X_test = test_data[["lastmonth_activity", "lastyear_activity", "number_of_employees"]]
 
-    return model_.predict(sc_.transform(data))
+    return model_.predict(sc_.transform(X_test))
 
 ##################Function to get summary statistics
-def dataframe_summary(data: pd.DataFrame) -> List[Tuple[float, float, float]]:
-    #calculate summary statistics here
+def dataframe_summary() -> List[Tuple[float, float, float]]:
+    
+    data = pd.read_csv(dataset_csv_path + "/" + "finaldata.csv")
+    # calculate summary statistics here
     # https://stackoverflow.com/questions/25039626/how-do-i-find-numeric-columns-in-pandas
     data_numeric = data.select_dtypes(include=np.number)
     list_stats = list()
@@ -39,24 +48,38 @@ def dataframe_summary(data: pd.DataFrame) -> List[Tuple[float, float, float]]:
 
     return list_stats
 
+##################Function to check for missing data
+def missing_data():
+
+    data = pd.read_csv(dataset_csv_path + "/" + "finaldata.csv")
+    return ((pd.isnull(data).sum())/len(data)).values
+
+
 ##################Function to get timings
 def execution_time():
     #calculate timing of training.py and ingestion.py
-    return #return a list of 2 timing values in seconds
+    start_tr = timeit.default_timer()
+    train_model()
+    end_tr = timeit.default_timer()
+
+    start_ing = timeit.default_timer()
+    merge_multiple_dataframe()
+    end_ing = timeit.default_timer()
+    return [end_tr - start_tr, end_ing - start_ing]
 
 ##################Function to check dependencies
 def outdated_packages_list():
-    #get a list of 
+    outdated_packages = subprocess.check_output(['pip', 'list','--outdated'])
+    df_ = pd.read_csv(BytesIO(outdated_packages), sep = "\t")
+    
+    return df_
 
 
 if __name__ == '__main__':
     model_predictions()
     dataframe_summary()
+    missing_data()
     execution_time()
     outdated_packages_list()
-
-
-
-
 
     
