09db4a8767aa0f83de270ecce7b48dbddc534099
Luis Melgar <luis.melgar@scrm.lidl>
2021-11-19 19:17:07 +0100

    changed app

--- app.py
+++ app.py
@@ -2,11 +2,12 @@ from flask import Flask, session, jsonify, request
 import pandas as pd
 import numpy as np
 import pickle
-import create_prediction_model
-import diagnosis 
-import predict_exited_from_saved_model
+#import create_prediction_model
+#import diagnosis 
+#import predict_exited_from_saved_model
 from scoring import score_model
-import diagnostics
+from diagnostics import model_predictions, dataframe_summary
+from diagnostics import missing_data, execution_time, outdated_packages_list
 import json
 import os
 
@@ -20,34 +21,34 @@ with open('config.json','r') as f:
     config = json.load(f) 
 
 dataset_csv_path = os.path.join(config['output_folder_path']) 
-
+test_data_path = os.path.join(config['test_data_path']) 
 prediction_model = None
 
 
 #######################Prediction Endpoint
-@app.route("/prediction", methods=['POST','OPTIONS'])
-def predict():    
-    # TODO: Fill    
+@app.route("/prediction")#, methods=['POST','OPTIONS'])
+def predict():
+    data = pd.read_csv(test_data_path + "/" + "testdata.csv")
     #call the prediction function you created in Step 3
-    return None #add return value for prediction outputs
+    return str(model_predictions(data)) #add return value for prediction outputs
 
 #######################Scoring Endpoint
 @app.route("/scoring", methods=['GET','OPTIONS'])
-def stats():        
+def scoring():        
     #check the score of the deployed model
-    return score_model(False)
+    return str(score_model(False))
 
 #######################Summary Statistics Endpoint
-@app.route("/summarystats", methods=['GET','OPTIONS'])
+@app.route("/summarystats")#, methods=['GET','OPTIONS'])
 def stats():        
     #check means, medians, and modes for each column
-    return diagnostics.dataframe_summary()
+    return dataframe_summary()
 
 #######################Diagnostics Endpoint
 @app.route("/diagnostics", methods=['GET','OPTIONS'])
-def stats():        
+def diagnostics():        
     #check timing and percent NA values
-    return diagnostics.missing_data(), diagnostics.execution_time(), diagnostics.outdated_packages_list()
+    return missing_data(), execution_time(), outdated_packages_list()
 
 if __name__ == "__main__":    
     app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
